97abbb07a4ba58f0dfab14edceea0ca9b3a7bd46
shakeerprince <shakeerprince@users.noreply.github.com>
2026-02-13 18:37:35 +0530

    fix: remove conflicting ad scripts for AdSense approval

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,14 +38,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-        <Script
-          src="https://pl28631767.effectivegatecpm.com/40/81/3c/40813ca1b2554767cc140e2d0d488096.js"
-          strategy="afterInteractive"
-        />
-        <Script
-          src="https://pl28632078.effectivegatecpm.com/f5/74/40/f57440145e97f106242572da533dde13.js"
-          strategy="afterInteractive"
-        />
         <ClientProviders>
           <Navbar />
           <main className="flex-1">
